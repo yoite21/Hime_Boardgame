a97b2856d75699977c35729c01fd74085b21a120
yoite.kim <yoite.kim@5minlab.com>
2017-08-04 17:45:00 +0900

    테이블, 카드 에셋 추가

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+	void Awake() {
+		GetComponent<Renderer> ().material.color = Color.white;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
